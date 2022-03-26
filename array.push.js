3a257fc0e6c616581560cea98d0566295baedbc4
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-03-26 22:55:01 +0600

    learn javascript array.push() method

---
+++ array.push.js
@@ -0,0 +1,15 @@
+/*
+The push() method adds new items to the end of an array.
+
+The push() method changes the length of the array.
+
+The push() method returns the new length.
+
+Syntax
+array.push(item1, item2, ..., itemX)
+*/
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const newNumbers = numbers.push(7, 9, 10);
+console.log(numbers);
+console.log(newNumbers);
