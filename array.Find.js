8a21e85a6cf559db5061345120590904e376eb90
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-03-26 17:31:08 +0600

    Learn javascript array.Find method.

---
+++ array.Find.js
@@ -0,0 +1,14 @@
+/*
+
+1) The find() method does not change the original array.
+2) The find() method returns the value of the first element that passes a test.
+3) The find() method executes a function for each array element.
+4) The find() method retuns undefined if no elements are found.
+5) The find() method does not execute the function for empty elements.
+
+syntax : array.find(function(currentValue, index, arr),thisValue)
+*/
+
+const number = [1,2,3,4,5,6,7]
+const result = number.find(number => number > 6)
+console.log(result);
