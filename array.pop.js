125228c817317d42b240b5b1f22a75ebaf5bf909
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-03-27 11:18:43 +0600

    Javascript array.pop() method.

---
+++ array.pop.js
@@ -0,0 +1,17 @@
+/*
+The pop() method removes (pops) the last element of an array.
+
+The pop() method changes the original array.
+
+The pop() method returns the removed element.
+*/
+
+const fruits = ['apple', 'banana', 'mango', 'orange', 'pineapple']
+const fruit = fruits.pop()
+console.log(fruit);
+console.log(fruits); // output: pineapple
+
+const names = []
+const name = names.pop()
+console.log(name); // output: undefined
+
