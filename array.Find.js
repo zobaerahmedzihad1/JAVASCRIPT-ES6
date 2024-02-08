c60915fafc5e2f0b0d5bd20a97401fb3bb75dced
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2024-02-08 12:33:08 +0600

    Refactor array.find method

--- array.Find.js
+++ array.Find.js
@@ -3,7 +3,7 @@
 1) The find() method does not change the original array.
 2) The find() method returns the value of the first element that passes a test.
 3) The find() method executes a function for each array element.
-4) The find() method retuns undefined if no elements are found.
+4) The find() method return undefined if no elements are found.
 5) The find() method does not execute the function for empty elements.
 
 syntax : array.find(function(currentValue, index, arr),thisValue)
@@ -11,12 +11,12 @@ syntax : array.find(function(currentValue, index, arr),thisValue)
 
 const number = [1,2,3,4,5,6,7]
 const result = number.find(number => number > 6)
-console.log(result); // outpute : 7
+console.log(result); // output : 7
 
 
 const number2 = [5,3,4,6,8]
 const result2 = number2.find(number => number2 > 40)
-console.log(result2); // outpute :  undefined
+console.log(result2); // output :  undefined
 
 const number3 = []
 const result3 = number3.find(number => number3 < 10)
